80b924b6769aa6ca24d2a33c81a957b03f4f3e3c
Vpsush <vpsush2023@gmail.com>
2023-07-23 22:41:59 +0300

    1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryContainer = document.querySelector(".gallery");
+const gallaryMarkUp = createGalleryItemsLibrary(galleryItems);
+
+gallaryContainer.insertAdjacentHTML("beforeend", gallaryMarkUp);
+
+function createGalleryItemsLibrary(galleryItems) {
+  const gallery = galleryItems
+    .map(({ preview, original, description }) => {
+      return ` 
+        <li class = "pictures">
+        <a class="link" 
+        href="${preview}">
+        <img class= "images"
+        alt = "${description}"
+        width="360"/>
+        </a>
+     </li>`;
+    })
+    .join("");
+  console.log(gallery);
+}
+// console.log(galleryItems);
